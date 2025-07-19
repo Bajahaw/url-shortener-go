61755cf6f5f6ee664e68d874e173955f239420e8
Radhi Bajahaw <me@radhi.tech>
2025-07-20 01:06:05 +0200

    feat: create table function

--- cmd/Database.go
+++ cmd/Database.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"context"
-	"database/sql"
 	"github.com/jackc/pgx/v5"
 	"os"
+	"strings"
 )
-
-var db sql.DB
 
 const (
 	DatabaseUrl = "postgres://user:pass@localhost:5432/urls?sslmode=disable"
@@ -22,4 +20,19 @@ func ConnectDB() {
 	}
 	defer conn.Close(ctx)
 	log.Println("DB Connected Successfully")
+	createTable(ctx, conn)
+}
+
+func createTable(ctx context.Context, conn *pgx.Conn) {
+	sql := "CREATE TABLE urls (id VARCHAR(10) PRIMARY KEY, url TEXT NOT NULL);"
+	_, err := conn.Exec(ctx, sql)
+	if err != nil {
+		if strings.Contains(err.Error(), "42P07") {
+			log.Println("Table urls already exist!")
+		} else {
+			log.Println("Failed to create table:", err)
+		}
+	} else {
+		log.Println("Table created")
+	}
 }
